fb993ed2a2dda414d282800a1dfd793cee14fd9a
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2023-09-17 10:39:03 +0200

    Update change_saal.js

--- js/change_saal.js
+++ js/change_saal.js
@@ -7,3 +7,23 @@ abmeldenButton.addEventListener("click", function () {
   // Weiterleitung zur Zielseite
   window.location.href = targetURL;
 });
+
+const Saal1Button = document.getElementById("Saal1");
+
+Saal1Button.addEventListener("click", function () {
+  // Hier die URL der Zielseite einfügen, zu der Sie weiterleiten möchten
+  const targetURL = "Saal1.html"; // Ändern Sie dies entsprechend Ihrer Zielseite
+
+  // Weiterleitung zur Zielseite
+  window.location.href = targetURL;
+});
+
+const Saal2Button = document.getElementById("Saal2");
+
+Saal2Button.addEventListener("click", function () {
+  // Hier die URL der Zielseite einfügen, zu der Sie weiterleiten möchten
+  const targetURL = "Saal2.html"; // Ändern Sie dies entsprechend Ihrer Zielseite
+
+  // Weiterleitung zur Zielseite
+  window.location.href = targetURL;
+});
